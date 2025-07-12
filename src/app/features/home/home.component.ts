aa5bfa3881fc75ae199ec47eca2187f4c08d6884
AdityaLahabar <adityalahabar@gmail.com>
2025-07-13 00:13:57 +0530

    Added transition home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
@@ -21,6 +21,21 @@ import { AuthService } from '../../core/services/auth.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
  constructor(public authService: AuthService) {}
+  backgroundImages: string[] = [
+   'https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1547248/pexels-photo-1547248.jpeg',
+    'https://images.pexels.com/photos/260447/pexels-photo-260447.jpeg',
+    'https://images.pexels.com/photos/28061/pexels-photo.jpg'
+  ];
+  currentIndex = 0;
+  currentBackground = this.backgroundImages[0];
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.backgroundImages.length;
+      this.currentBackground = this.backgroundImages[this.currentIndex];
+    }, 5000); // every 5 seconds
+  }
 }
